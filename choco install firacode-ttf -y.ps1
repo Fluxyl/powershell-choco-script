d9ef1ef799cd99bb97ee52d5030208016c989c19
Flux <91804289+Fluxyl@users.noreply.github.com>
2021-10-20 03:43:49 -0500

    Add files via upload

---
+++ choco install firacode-ttf -y.ps1
@@ -0,0 +1,47 @@
+choco install firacode-ttf -y
+choco install firefox -y
+choco install notepadplusplus.install -y
+choco install 7zip.install -y
+choco install vlc -y
+choco install python3 -y
+choco install git.install -y
+choco install zoom -y
+choco install gimp -y
+choco install vscode -y
+choco install thunderbird -y
+choco install spotify -y
+choco install virtualbox -y
+choco install irfanview -y
+choco install audacity -y
+choco install everything -y
+choco install brave -y
+choco install gh -y
+choco install MusicBee -y
+choco install AIMP -y
+choco install discord -y
+choco install steam -y
+choco install telegram -y
+choco install qBittorent -y
+choco install pia -y
+choco install msiafterburner -y
+choco install blender -y
+choco install eartrumpet -y
+choco install Keypirinha -y
+choco install cpu-z.install -y
+choco install hwinfo.install -y
+choco install procexp -y
+choco install wiztree -y
+choco install obs-studio -y
+choco install autoruns -y
+choco install tor-browser -y
+choco install dolphin -y
+choco install cmder -y
+choco install processhacker -y
+choco install mpv -y
+
+# Install the following manually..
+# Java
+# Metro4Steam
+# Office
+# VIA
+# Ubuntu/Linux in general to a Virtual Machine
